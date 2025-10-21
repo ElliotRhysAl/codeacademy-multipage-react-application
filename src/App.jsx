c88a80329945dfb4878b560fe63963073728b013
Elliot Allen <elliotallendev@outlook.com>
2025-10-21 13:47:17 +0000

    did some stuff - looking at templates from Tailwind Plus/Tailwind UI. Haven't got a solid plan of what I want to build yet but just throwing bits together

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import './style.css'
 import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      <div id='logo'>
         <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={viteLogo} className="logo vite" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router';
+
+import './style.css'
 import './index.css'
 import App from './App.jsx'
+import Nav from './Nav.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>  
+      <Nav />
+      <div className="pt-16">
+        <Routes>
+          <Route path="/" element={<App />} />
+          {/* Fallback route for 404 Not Found */}
+          <Route path="/404" element={<h1>Page Not Found</h1>} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   </StrictMode>,
 )
